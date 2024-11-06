a2173ff463c4eab91f429a0ca05a1584a01e0a27
Miguel <miguel.vanegas-c@mail.escuelaing.edu.co>
2024-11-06 00:19:10 -0500

    finish proyect

--- proyectoFinal/src/puzzle/Main.java
+++ proyectoFinal/src/puzzle/Main.java
@@ -3,7 +3,10 @@ package puzzle;
 import javax.swing.*;
 
 public class Main {
-    public static void main(String[] args) {acceptanceTestSolveAndSimulateThePuzzleByMiguel();}
+    public static void main(String[] args) {
+        acceptanceTestSolveAndSimulateThePuzzleByMiguel();
+        acceptanceTestAllTheGlueByAllan();
+    }
     private static void acceptanceTestSolveAndSimulateThePuzzleByMiguel() {
         char[][] starting = {{'.', 'b', '.', '.', '.', 'm'},
                 {'.', '.', 'b', '.', '.', '.'},
@@ -65,5 +68,70 @@ public class Main {
         char[][] contestCaseTwoStarting = {{'y', 'r', '.'}, {'.', '.', 'b'}, {'r', 'y', '.'}, {'b', '.', '.'}};
         char[][] contestCaseTwoEnding = {{'.', '.', '.'}, {'.', '.', 'b'}, {'.', 'r', 'y'}, {'b', 'y', 'b'}};
         puzzleContest.simulate(contestCaseTwoStarting, contestCaseTwoEnding);
+        JOptionPane.showMessageDialog(null, "Finaliza la primera prueba de aceptacion");
+
+    }
+    private static void acceptanceTestAllTheGlueByAllan(){
+        char[][] starting = {{'.', 'b', '.', '.', '.', 'm'},
+                {'.', '.', 'b', '.', '.', '.'},
+                {'.', '.', 'y', '.', '.', 'b'},
+                {'g', '.', '.', '.', '.', '.'},
+                {'.', '.', 'g', '.', '.', 'b'},
+                {'.', '.', '.', '.', '.', '.'},
+                {'b', '.', '.', 'm', '.', 'y'}};
+        char[][] ending = {{'r', 'b', 'b', '.', '.', '.'},
+                {'r', '.', '.', '.', '.', '.'},
+                {'r', '.', '.', '.', '.', '.'},
+                {'g', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.'}};
+        Puzzle puzzle = new Puzzle(starting,ending);
+        puzzle.makeVisible();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            System.out.println("El hilo fue interrumpido.");
+        }
+        puzzle.tilt('r');
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            System.out.println("El hilo fue interrumpido.");
+        }
+        JOptionPane.showMessageDialog(null, "se añade el primer pegante en la fila uno columna 6");
+        puzzle.addGlue(1,6,1);
+        puzzle.tilt('d');
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            System.out.println("El hilo fue interrumpido.");
+        }
+        JOptionPane.showMessageDialog(null, "se añade el segundo pegante en la fila 4 columna 6");
+        puzzle.addGlue(4,6,1);
+        puzzle.tilt('l');
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            System.out.println("El hilo fue interrumpido.");
+        }
+        puzzle.tilt('u');
+        JOptionPane.showMessageDialog(null, "se eliminan los pegantes");
+        puzzle.deleteGlue(1,2);
+        puzzle.deleteGlue(3,2);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            System.out.println("El hilo fue interrumpido.");
+        }
+        puzzle.tilt('d');
+        JOptionPane.showMessageDialog(null, "se añade otro pegante pero ahora fragil");
+        puzzle.addGlue(4,2,2);
+        puzzle.tilt('u');
+        JOptionPane.showMessageDialog(null, "se comprueba que se halla roto el pegante despues del movimiento");
+        puzzle.tilt('u');
+        JOptionPane.showMessageDialog(null, "finaliza la prueba dos");
+        puzzle.tilt('r');
+        puzzle.makeInvisible();
     }
 }
